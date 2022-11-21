8938b4d7044d406bf5ea340f58dab6510c0bbe9b
Plumbheater <plumbheater@yandex.ru>
2022-11-21 23:20:18 +0300

    fix2 commit

--- src/test/java/CashBackHackerTest.java
+++ src/test/java/CashBackHackerTest.java
@@ -4,12 +4,27 @@ import org.testng.annotations.Test;
 public class CashBackHackerTest {
 
     @Test
-    public void remain() {
+    public void AmountZero() {
         CashBackHackerService service = new CashBackHackerService();
-        int amount = 900;
+        int amount = 0;
         int actual = service.remain(amount);
-        int expected = 100;
+        int expected = 1000;
         Assert.assertEquals(actual, expected);
     }
-
+    @Test
+    public void AmountThousand() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void Amount999() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        Assert.assertEquals(actual, expected);
+    }
 }
